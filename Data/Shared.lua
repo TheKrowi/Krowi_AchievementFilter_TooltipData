e408a76c6e0b85da4171c1c4622b2b7865aa4de5
Krowi <Krowi@Home>
2024-04-27 08:30:23 +0200

    added util module

--- Data/Shared.lua
+++ Data/Shared.lua
@@ -1,10 +1,10 @@
 local _, addon = ...;
+addon.Shared = {};
+local shared = addon.Shared;
 
 local N, NT, type, fac, txt = addon.AddTooltipData, addon.AddTooltipDataTable, addon.TooltipDataType, addon.Faction, addon.TooltipDataText;
 
-addon.Shared = {};
-
-addon.Shared.ToAllTheSquirrelsPropertiesGroup = {
+shared.ToAllTheSquirrelsPropertiesGroup = {
     ObjectType = type.Unit,
     NotCompletedText = txt[1],
     CompletedText = txt[2]
