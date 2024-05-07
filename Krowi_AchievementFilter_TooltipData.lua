846b847fdb8bb3d602c4fe3ecf3fd330d6cf1d84
Krowi <Krowi@Home>
2024-05-07 22:49:29 +0200

    cleanup

--- Krowi_AchievementFilter_TooltipData.lua
+++ Krowi_AchievementFilter_TooltipData.lua
@@ -1,30 +1,8 @@
 local _, addon = ...;
 
--- [[ Load addon ]] --
--- local loadHelper = CreateFrame("Frame");
--- loadHelper:RegisterEvent("ADDON_LOADED");
--- loadHelper:RegisterEvent("PLAYER_LOGIN");
-
 addon.AddTooltipData = KrowiAF.AddTooltipData;
 addon.AddTooltipDataTable = KrowiAF.AddTooltipDataTable;
 addon.AddTooltipDataTableAchievements = KrowiAF.AddTooltipDataTableAchievements;
 addon.TooltipDataType = KrowiAF.GetTooltipDataTypes();
 addon.Faction = KrowiAF.GetGetFactions();
 addon.TooltipDataText = KrowiAF.GetTooltipDataTexts();
-
--- local function LoadData()
-
--- end
-
--- function loadHelper:OnEvent(event, arg1, arg2)
---     if event == "ADDON_LOADED" then
---         if arg1 == "Krowi_AchievementFilter" then
---             LoadData();
---         end
---     elseif event == "PLAYER_LOGIN" then
---         if IsAddOnLoaded("Krowi_AchievementFilter") then
---             LoadData();
---         end
---     end
--- end
--- loadHelper:SetScript("OnEvent", loadHelper.OnEvent);
